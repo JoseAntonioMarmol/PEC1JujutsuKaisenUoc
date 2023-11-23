fe0c90dd54859ca9b8195c5ef35702624d72dd7a
Jose Antonio Marmol <joseantoniomarmol@sothis.tech>
2023-11-23 22:04:08 +0100

    Add Prettier and ESLint configuration files

--- src/app.js
+++ src/app.js
@@ -1,42 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-      var observer = new IntersectionObserver(function(entries) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.5 });
-
-    var animatedElements = document.querySelectorAll('.index-title, p, .chars');
-
-    animatedElements.forEach(function(element) {
-        observer.observe(element);
-    });
-
-    setTimeout(function() {
-        var loadingScreen = document.querySelector('.loading-screen');
-        var mainContainer = document.querySelector('.main_container');
-
-        if (loadingScreen && mainContainer) {
-            loadingScreen.style.opacity = '0';
-            loadingScreen.style.visibility = 'hidden';
-            mainContainer.style.opacity = "1";
-            mainContainer.style.visibility = 'visible';
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
         }
-    }, 3000);
-});
-document.querySelector('.side-menu').addEventListener('click', function() {
-    var sideMenu = document.querySelector('.side-menu');
-    var iframe = document.getElementById('ytIframe');
+      });
+    },
+    { threshold: 0.5 }
+  );
 
-    sideMenu.classList.toggle('open');
+  var animatedElements = document.querySelectorAll(".index-title, p, .chars");
 
-    if (sideMenu.classList.contains('open')) {
-      iframe.style.display = 'none';
-    } else {
-      iframe.style.display = 'block';
+  animatedElements.forEach(function (element) {
+    observer.observe(element);
+  });
+
+  setTimeout(function () {
+    var loadingScreen = document.querySelector(".loading-screen");
+    var mainContainer = document.querySelector(".main_container");
+
+    if (loadingScreen && mainContainer) {
+      loadingScreen.style.opacity = "0";
+      loadingScreen.style.visibility = "hidden";
+      mainContainer.style.opacity = "1";
+      mainContainer.style.visibility = "visible";
     }
+  }, 3000);
+});
+document.querySelector(".side-menu").addEventListener("click", function () {
+  var sideMenu = document.querySelector(".side-menu");
+  var iframe = document.getElementById("ytIframe");
+
+  sideMenu.classList.toggle("open");
+
+  if (sideMenu.classList.contains("open")) {
+    iframe.style.display = "none";
+  } else {
+    iframe.style.display = "block";
+  }
 });
